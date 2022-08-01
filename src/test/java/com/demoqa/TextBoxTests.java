d044f9e13b22fe6218ded81d0fddb25e59557baa
Andry <strawer@mail.ru>
2022-08-01 09:30:24 +0300

    1. Локаторы по id теперь указаны через "#name" 2. Убраны лишние пробелы 3. Переменные применяются только для ситуации, когда значение используется несколько раз.

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -22,69 +22,34 @@ public class TextBoxTests {
 //        Configuration.browser = "opera";
         Configuration.browserSize = "1920x1080";
     }
-
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        SelenideElement firstName = $("[id=firstName]");
-        firstName.setValue("Andrey");
 
-        SelenideElement lastName = $("[id=lastName]");
-        lastName.setValue("Zaytsev");
-
-        SelenideElement userEmail = $("[id=userEmail]");
-        userEmail.setValue("test@example.com");
-
-        SelenideElement genderRadio = $("[for='gender-radio-1']");
-        genderRadio.click();
-
-        SelenideElement userNumber = $("[id=userNumber]");
-        userNumber.setValue("9100000000");
-
-        SelenideElement dateOfBirthInput =  $("[id=dateOfBirthInput]");
-        dateOfBirthInput.click();
-
-        SelenideElement yearSelect = $(".react-datepicker__year-select");
-        yearSelect.selectOptionContainingText("2000");
-
-        SelenideElement monthSelect = $(".react-datepicker__month-select");
-        monthSelect.selectOptionContainingText("Oct");
-
-
-        SelenideElement daySelect =  $(".react-datepicker__day--001");
-        daySelect.click();
-
+        $("#firstName").setValue("Andrey");
+        $("#lastName").setValue("Zaytsev");
+        $("#userEmail").setValue("test@example.com");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("9100000000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionContainingText("2000");
+        $(".react-datepicker__month-select").selectOptionContainingText("Oct");
+        $(".react-datepicker__day--001").click();
         SelenideElement subjectsInput = $("[id=subjectsInput]");
         subjectsInput.click();
         subjectsInput.setValue("English").pressEnter();
         subjectsInput.setValue("Computer Science").pressEnter();
         subjectsInput.setValue("Math").pressEnter();
-
-        SelenideElement hobbiesCheckbox1 = $("[for='hobbies-checkbox-1']");
-        hobbiesCheckbox1.click();
-
-        SelenideElement hobbiesCheckbox2 = $("[for='hobbies-checkbox-2']");
-        hobbiesCheckbox2.click();
-
-        SelenideElement hobbiesCheckbox3 = $("[for='hobbies-checkbox-3']");
-        hobbiesCheckbox3.click();
-
-        SelenideElement uploadPicture = $("[id=uploadPicture]");
-        uploadPicture.uploadFile(new File("src/test/resources/test_file.png"));
-
-        SelenideElement currentAddress = $("[id=currentAddress");
-        currentAddress.setValue("Kaluga");
-
-
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-2']").click();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/test_file.png"));
+        $("[id=currentAddress").setValue("Kaluga");
         $(byText("Select State")).click();
         $(byText("NCR")).click();
         $(byText("Select City")).click();
         $(byText("Noida")).click();
-
-        SelenideElement submit = $(byText("Submit"));
-        submit.click();
-
-
+        $(byText("Submit")).click();
 
         SelenideElement modalBody = $(".modal-body");
         modalBody.shouldHave(text("Andrey Zaytsev"));
@@ -97,8 +62,5 @@ public class TextBoxTests {
         modalBody.shouldHave(text("test_file.png"));
         modalBody.shouldHave(text("Kaluga"));
         modalBody.shouldHave(text("NCR Noida"));
-
-
     }
-
 }
