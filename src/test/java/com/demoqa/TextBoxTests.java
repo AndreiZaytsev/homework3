0217f2edbf245a21bc5c67101b6737eea7e68345
Andry <strawer@mail.ru>
2022-08-10 14:31:54 +0300

    Разнообразные улучшения теста, добавлена проверка

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -25,35 +26,35 @@ public class TextBoxTests {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Andrey");
         $("#lastName").setValue("Zaytsev");
         $("#userEmail").setValue("test@example.com");
-        $("[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("9100000000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionContainingText("2000");
         $(".react-datepicker__month-select").selectOptionContainingText("Oct");
-        $(".react-datepicker__day--001").click();
-        SelenideElement subjectsInput = $("[id=subjectsInput]");
-        subjectsInput.click();
-        subjectsInput.setValue("English").pressEnter();
-        subjectsInput.setValue("Computer Science").pressEnter();
-        subjectsInput.setValue("Math").pressEnter();
-        $("[for='hobbies-checkbox-1']").click();
-        $("[for='hobbies-checkbox-2']").click();
-        $("[for='hobbies-checkbox-3']").click();
+        $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/test_file.png"));
-        $("[id=currentAddress").setValue("Kaluga");
+        $("#currentAddress").setValue("Kaluga");
         $(byText("Select State")).click();
         $(byText("NCR")).click();
         $(byText("Select City")).click();
         $(byText("Noida")).click();
         $(byText("Submit")).click();
 
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
         SelenideElement modalBody = $(".modal-body");
         modalBody.shouldHave(text("Andrey Zaytsev"), text("test@example.com"), text("Male"), text("9100000000"),
-                text("01 October,2000"), text("English, Computer Science, Maths"), text("Sports, Reading, Music"),
+                text("01 October,2000"), text("Maths, English, Computer Science"), text("Sports, Reading"),
                 text("test_file.png"), text("Kaluga"), text("NCR Noida"));
 
     }
