77b0e6b01d35c6548ff02c4856f111596778b69d
Andry <strawer@mail.ru>
2022-09-08 17:05:40 +0300

    small changes

--- src/test/java/com/demoqa/helpers/Attach.java
+++ src/test/java/com/demoqa/helpers/Attach.java
@@ -5,13 +5,13 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
-
 
 public class Attach {
     @Attachment(value = "{attachName}", type = "image/png")
@@ -35,6 +35,11 @@ public class Attach {
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
     }
+
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
@@ -51,9 +56,5 @@ public class Attach {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static String getSessionId(){
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
